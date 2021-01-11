117354315f65aa8a407d52d19d58cf90a24d2555
galiya <gkey-587@mail.ru>
2021-01-11 15:55:01 +0500

    changes

--- app.js
+++ app.js
@@ -18,51 +18,35 @@ document.body.addEventListener('click', event => {
 const popUp = document.querySelector('.pop-up'),
     sendBtn = document.querySelector('#send-btn');
 
-// sendBtn.onclick = function() {
-//     popUp.classList.toggle('display-none');
-// }
+sendBtn.onclick = function() {
+    popUp.style.display = 'block';
+};
 
-// sendBtn.onclick = function() {
-//     popUp.style.display = 'none';
-// };
-
-
-
-// $('.pop-up').toggle( "slow");
-
-// Отправка заявки 
-$(document).ready(function() {
-	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
-		if (document.form.name.value == '' || document.form.email.value == '' ) {
-			valid = false;
-			return valid;
-		}
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$('.pop-up').fadeIn();
-			$(this).find('input').val('');
-			$('#form').trigger('reset');
-		});
-		return false;
-	});
-});
-
-// Закрыть попап «спасибо»
-// $('.popUp').click(function() { // по клику на крестик
-// 	$('.popUp').fadeOut();
+// // Вместо класса можно использовать data-атрибуты: "[data-clear-button]"
+// document.querySelectorAll(".clear-button")
+// .forEach(function (elem) {
+//   elem.onclick = function (e) {
+//     // Если необходимо предотвратить/отменить событие по умолчанию,
+//     // то необходимо вызвать метод preventDefault у события
+//     // https://developer.mozilla.org/ru/docs/Web/API/Event/preventDefault
+//     // e.preventDefault();
+//     // если необходимо также предотвратить дальнейшее "всплытие" события,
+//     // то необходимо вызвать метод stopPropagation у события
+//     // https://developer.mozilla.org/ru/docs/Web/API/Event/stopPropagation
+//     // e.stopPropagation();
+//     let selector = this.dataset.clearSelector;
+//     document.querySelectorAll(selector)
+//     .forEach(function (item) {
+//       item.value = "";
+//     });
+//   };
 // });
 
-$(document).mouseup(function (e) { // по клику вне попапа
-    var popup = $('.popup');
-    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-        $('.popUp').fadeOut();
-    }
+
+document.body.addEventListener('click', event => {
+    if (!event.target.closest('#send-btn')) {
+      console.log('клик не внутри pop-up'); 
+      popUp.style.display = 'none';
+	}
 });
 
-// Маска ввода номера телефона (плагин maskedinput)
-$(function($){
-	$('[name="phone"]').mask("+7(999) 999-9999");
-});
